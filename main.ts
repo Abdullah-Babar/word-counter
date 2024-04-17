41cf0f339b9b03b28cbd9121f0be44d4e2362423
Abdullah Babar <abdullahbabar994@gmail.com>
2024-04-17 22:16:09 +0500

    my word counter project

---
+++ main.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+console.log("\nWelcome! to my word counter project\n");
+
+
+const answers: {
+    Sentence: string
+} = await  inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence to count the word:"
+    }
+])
+
+let words = answers.Sentence.trim().split(" ")
+
+console.log(words);
+
+console.log(`Your sentence word count is ${words.length}`);
+
+console.log("\nThanks! for using my word counter project");
